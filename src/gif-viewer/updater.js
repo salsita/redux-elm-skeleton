b54a7d5c2edf8e4646f0da7e8e7ef0a1b99dc9ce
Tomas Weiss <tomasw@salsitasoft.com>
2016-05-11 16:48:14 +0200

    gotten rid of regresive tests

--- src/gif-viewer/updater.js
+++ src/gif-viewer/updater.js
@@ -4,15 +4,15 @@ import { call, put, select } from 'redux-saga/effects';
 
 import * as Effects from './effects';
 
-const getTopic = model => model.topic;
+export const getTopic = model => model.topic;
 
-function* fetchGif() {
+export function* fetchGif() {
   const topic = yield select(getTopic);
   const url = yield call(Effects.fetchGif, topic);
   yield put({ type: 'NewGif', url });
 }
 
-function* saga() {
+export function* saga() {
   yield* fetchGif();
   yield* takeEvery('RequestMore', fetchGif);
 }

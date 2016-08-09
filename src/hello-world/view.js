0a22131985028a853f32a81814867f434e34d01c
Tomas Weiss <tomasw@salsitasoft.com>
2016-08-09 14:22:26 +0200

    upgrading dependencies

--- src/hello-world/view.js
+++ src/hello-world/view.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { view } from 'redux-elm';
 
-export default ({ model, dispatch }) => {
+export default view(({ model, dispatch }) => {
   if (model.greeted) {
     return <div>Hello World!</div>;
   } else {
     return <button onClick={() => dispatch({ type: 'SayHi' })}>Say Hi</button>;
   }
-};
+});

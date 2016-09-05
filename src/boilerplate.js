2789f8251c6bc7580e3d50884b36c7bfcfc6160d
Tomas Weiss <tomasw@salsitasoft.com>
2016-09-05 10:40:25 +0200

    HMR

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -4,7 +4,7 @@ import { createStore, compose } from 'redux';
 import { Provider, connect } from 'react-redux';
 import reduxElm from 'redux-elm';
 
-export default (containerDomId, View, updater) => {
+export default (containerDomId, updater) => {
   const storeFactory = compose(
     reduxElm,
     window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -12,13 +12,21 @@ export default (containerDomId, View, updater) => {
 
   const store = storeFactory(updater);
 
-  const ConnectedView = connect(appState => ({
-    model: appState
-  }))(View);
 
-  render((
-    <Provider store={store}>
-      <ConnectedView />
-    </Provider>
-  ), document.getElementById(containerDomId));
-}
+  const doRender = View => {
+    const ConnectedView = connect(appState => ({
+      model: appState
+    }))(View);
+
+    render((
+      <Provider store={store}>
+        <ConnectedView />
+      </Provider>
+    ), document.getElementById(containerDomId));
+  };
+
+  return {
+    doRender,
+    store
+  };
+};
--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import run from './boilerplate';
 import view from './hello-world/view';
 import updater from './hello-world/updater';
 
-run('app', view, updater);
+const { doRender, store } = run('app', updater);
+
+doRender(view);
+
+if (module.hot) {
+  module.hot.accept('./hello-world/view', () => {
+    doRender(require('./hello-world/view').default);
+  });
+
+  module.hot.accept('./hello-world/updater', () => {
+    store.replaceReducer(require('./hello-world/updater').default);
+  });
+}
